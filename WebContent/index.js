9deb7ba8e4f6ea3370758073d26bf8e10fa9f0e3
fc <13297903879@163.com>
2017-02-05 23:12:04 +0800

    first commit

---
+++ WebContent/index.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+ //左边缩进按钮控制start
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+    $('#wrapper').toggleClass('toggled');
+    trigger.click(function () {
+      hamburger_cross();      
+    });
+ 
+    function hamburger_cross() {
+ 
+//      if (isClosed == true) {          
+//        overlay.hide();
+//        trigger.removeClass('is-open');
+//        trigger.addClass('is-closed');
+//        isClosed = false;
+//      } else {   
+//        overlay.show();
+//        trigger.removeClass('is-closed');
+//        trigger.addClass('is-open');
+//        isClosed = true;
+//      }
+  }
+   
+  $('.icon-flex').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });  
+  //缩进按钮控制结束
+  
+  //左边菜单展开控制start
+  $('.second-menu').css("display","none");  /*第一次隐藏所有的二级菜单*/ 
+  debugger;
+  $('.top-menu').click(function(){
+	  $(this).children("ul.second-menu").css("display","block");
+	  $(this).addClass('fa fa-angle-left');
+//	  $('.second-menu').css("display","block");
+  })
+});
